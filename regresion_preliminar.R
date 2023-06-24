4fac5edb4d891b3ba20b1ddaa5cfceebf3ace63d
Damian Piuselli <damianpiuselli@gmail.com>
2023-06-24 20:50:30 -0300

    cambiamos cosas en regresion_preliminar + imgs diapo

--- regresion_preliminar.R
+++ regresion_preliminar.R
@@ -11,27 +11,30 @@ glimpse(data) #Categoricas -> Economy_status_Developed, Economy_status_Developin
 
 data <- data %>% 
   mutate(GDP_per_capita = log(GDP_per_capita)) %>% 
-  rename(log_GDP = GDP_per_capita)
-
-
-## Separando los datos
-
-data_pred <- data %>% filter(Year != 2015) %>% select(-Region, -Country, -Year)
-
-data_test <- data %>% filter(Year == 2015) %>% select(-Region, -Country, -Year)
+  rename(log_GDP = GDP_per_capita) %>% 
+  select(-Economy_status_Developing, -Region, -Country, -Year)
 
 
 ## Primer modelo, todas las variables.
 
-mod_full = lm(data=data_pred, Life_expectancy ~ .)
+mod_full = lm(data=data, Life_expectancy ~ .)
 summary(mod_full)  
 
-#agrego prediciones al dataset de test (2015)
+## segundo modelo
 
-data_test <- add_predictions(data_test, mod_full, var="pred mod_full")
+data <- data %>% select(-Population_mln, -Measles, -Thinness_five_nine_years, -Diphtheria, -Under_five_deaths, -Incidents_HIV )
 
-#grafico  valor vs pred para el set de validacion. Linea de 45 grados seria la predicción ideal.
+mod_1 = lm(data=data, Life_expectancy ~ .)
+summary(mod_1) 
 
-data_test %>% ggplot(aes(y=Life_expectancy, x=`pred mod_full`))+
-  geom_point() + 
-  geom_abline(slope=1, intercept = 0, color="red")
+
+#tercer modelo. #todos los terminos de interaccion para chusmear.
+
+mod_2 = lm(data=data, Life_expectancy ~ .*.)
+summary(mod_2) 
+
+#cuarto modelo
+mod_3 = lm(data=data, Life_expectancy ~ . +Alcohol_consumption*log_GDP+ BMI*log_GDP)
+summary(mod_3) 
+
+
